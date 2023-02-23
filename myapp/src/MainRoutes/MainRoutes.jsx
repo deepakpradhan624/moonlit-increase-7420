4dbc70959b4a8bbaae3d231ae2ac9c0fea505c5d
Sudipta Pramanik <sudiptapramanik209@gmail.com>
2023-02-24 01:30:42 +0530

    login_signin_with_json server

--- myapp/src/MainRoutes/MainRoutes.jsx
+++ myapp/src/MainRoutes/MainRoutes.jsx
@@ -3,12 +3,16 @@ import HomePage from '../Product_Component/MenProduct/Pages/HomePage'
 // import ProductDetails from './ProductDetails'
 import { Routes, Route } from "react-router-dom";
 import WomenHomePage from '../Product_Component/WomenProduct/Pages/WomenHomePage';
+import Login from '../Components/signup_login/Login';
+import SignUp from '../Components/signup_login/SignUp';
 // import HomePage  from '../Product_Component/WomenProduct/Pages/WomenHomePage';
 
 const MainRoutes = () => {
   return (
     <Routes>
         <Route path="/menproduct" element={<HomePage/>} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
         <Route path="/womenproduct" element={<WomenHomePage/>} />
 
         
