3128e19adb793ac75e2bf855e4997e9886602230
deepakpradhan624 <pradhand92041@gmail.com>
2023-04-07 20:57:36 +0530

    added backend server

--- myapp/src/Redux/mensProduct/action.js
+++ myapp/src/Redux/mensProduct/action.js
@@ -19,7 +19,7 @@ export const productFailure = () => {
 export const getProduct=(param)=>(dispatch)=>{
 dispatch(productRequest())
 return axios
-.get("https://pink-shiny-bunny.cyclic.app/mens",param)
+.get("https://spring-bud-buffalo-robe.cyclic.app/mens",param)
 .then((res)=>{
     dispatch(productSuccess(res.data))
     console.log(res.data)
